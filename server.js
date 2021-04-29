458e1110b5f9ff47693e662084bbebbb02d6a215
Gregory Nguenang <njethur@gmail.com>
2021-04-28 19:28:06 -0500

    Updating files before deployment

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3007;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, async () => {
     console.log(`App listening on port ${PORT}!`);
